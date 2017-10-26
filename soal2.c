2d858a52be8f28a5ff1459ca2901d31152f8e913
chendrasena26 <chendrasena.oe@gmail.com>
2017-10-26 16:50:09 +0700

    soal dua

--- soal2.c
+++ soal2.c
@@ -4,21 +4,23 @@
 #include<unistd.h>
 pthread_t treat[10];
 char pem1[100], pem2[100];
-int lub[16], skor1=0, skor2=0;
-void namaewa() {
+int skor1=0, skor2=0, a, status;
+void *namaewa() {
+    status=0;
     printf("Masukkan nama pemain1: ");
     gets(pem1);
     printf("Masukkan nama pemain2: ");
     gets(pem2);
+    status++;
 }
 
 void cetakskor() {
     printf("Skor "); puts(pem1); printf(": %d\n",skor1);
     printf("Skor "); puts(pem2); printf(": %d\n",skor2);
 }
-void tpem1() {
+void *isi(int *lub) {
+    while(status!=1||status!=2) {}
     int jum, temp;
-        printf("Giliran "); puts(pem1);
     while(1) {
         printf("mau mengisi berapa lubang?? ");
         scanf("%d",&jum);
@@ -31,22 +33,26 @@ void tpem1() {
         scanf("%d",&temp);
         if(temp<1||temp>16) printf("posisi lubang tak tersedia, ulangi input\n");
         else {
-            lub[temp-1]=1;
-            break;
+            if(lub[temp-1]==1) printf("lubang telah terpasang MINE hehe\n");
+            else {
+                lub[temp-1]=1;
+                break;
+            }
         }
         }
     }
+    status++;
 }
 
-void tpem2() {
+void *tebak(int *lub) {
+    while(status!=3||status!=4) {}
     int tebak;
-    printf("Giliran "); puts(pem2);
     for(int i=0;i<4;i++) {
         printf("Masukkan posisi lubang yang ingin ditebak ");
         while(1) {
             scanf("%d",&tebak);
             if(tebak<1||tebak>16) {
-                printf("Posisi lubang tak ada\n");
+                printf("Posisi lubang tak ada, ulangi input \n");
             }
             else break;
         }
@@ -60,12 +66,49 @@ void tpem2() {
         }
         cetakskor();
     }
+    status++;
 }
 
+void *ceklub(int *lub) {
+    while(status!=5||status!=6) {}
+    for(int i=0;i<16;i++) {
+        if(lub[i]==1) a++;
+    }
+    status++;
+}
 
 int main()
 {
-    namaewa();
-    tpem1();
-    tpem2();
+    int lub1[16], lub2[16];
+    int err;
+    while(1) {
+            a=0;
+            //status=0;
+            err=pthread_create( &(treat[0]), NULL, &namaewa, NULL);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[1]), NULL, &isi, (int*) lub1);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[2]), NULL, &isi, (int*) lub2);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[3]), NULL, &tebak, (int*) lub1);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[4]), NULL, &tebak, (int*) lub2);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[5]), NULL, &ceklub, (int*) lub1);
+            if(err) printf("error ma bro\n");
+            err=pthread_create( &(treat[6]), NULL, &ceklub, (int*) lub2);
+            if(err) printf("error ma bro\n");
+
+            pthread_join(treat[0],NULL);
+            pthread_join(treat[1],NULL);
+            pthread_join(treat[2],NULL);
+            pthread_join(treat[3],NULL);
+            pthread_join(treat[4],NULL);
+            pthread_join(treat[5],NULL);
+            pthread_join(treat[6],NULL);
+            if(a==32||skor1==10||skor2==10) {
+            printf("permainan selesai. \n");
+            break;
+        }
+    }
 }
