ab9f295863bbdbe8552ad4b8c7dede495aa1e785
chendrasena26 <chendrasena.oe@gmail.com>
2017-10-26 17:26:48 +0700

    soal lima

---
+++ soal5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+pthread_t tid[100];
+
+char word[1500];
+char search[100];
+int i = 1;
+
+void *cek() {
+    FILE *f;
+        f=fopen("Novel.txt", "r");
+        scanf("%s", search);
+        while (!feof(f)) {
+        fscanf(f, "%s", word);
+            if (word==search){
+            printf("\n %i %s ", i, word);
+            i++;}
+        }
+        fclose(f);
+}
+int main()
+{
+
+}
