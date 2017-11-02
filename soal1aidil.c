f84525332c8e29e66b0fde590918bec269745503
chendrasena26 <chendrasena.oe@gmail.com>
2017-11-03 02:47:39 +0700

    soalsatubeli

--- soal1aidil.c
+++ soal1aidil.c
@@ -20,6 +20,19 @@ void main()
                     printf("Stok %s : %d\n",barang[i],setok[i]);
                 }
             }
+	    else if(masuk==2) {
+                printf("Mau beli yang mana dan jumlah berapa? ");
+                scanf("%s",&inputan); scanf("%d",&jumlah);
+                for(int j=0;j<6;j++) {
+                if(!strcmp(inputan,barang[j])) {
+                    if(setok[j]>=jumlah) {
+                        setok[j]=setok[j]-jumlah;
+                        printf("Pembelian berhasil! Sekarang stok %s menjadi %d\n",barang[j],setok[j]);
+                        }
+                    else printf("Barang di stok tidak cukup ma bro\n");
+                    }
+                }
+            }
 	}
         shmdt(setok);
         shmctl(shmid, IPC_RMID, NULL);
